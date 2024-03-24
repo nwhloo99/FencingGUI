15fe0c43d6eeb89f66dce84f76ed359f03bdd840
nwhloo99 <77220827+nwhloo99@users.noreply.github.com>
2024-03-25 01:21:04 +0800

    Add Progress count to scoreboard detection

--- mainApp.py
+++ mainApp.py
@@ -1,8 +1,16 @@
-import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QFileDialog
 from pathlib import Path
+
+from ultralytics import YOLO
+import sys
+import os
+import cv2
+from matplotlib import pyplot as plt
+import pandas as pd
+import io
+from contextlib import redirect_stdout
 
 from mainAppUI import Ui_MainWindow
 from masking import maskVideo
@@ -60,7 +68,32 @@ class MainWindow(QMainWindow):
             path = Path(self.ui.lineEdit_2.text())
             folder = path.parent
             try:
-                scoreboardDetection(folder)
+                model = YOLO('./train17/weights/last.pt')
+                mycwd = os.getcwd()
+                os.chdir(folder)
+
+                source = "./masked.mp4"
+                results = model(source, stream=True)
+
+                boxes = []
+                self.ui.scoreboard_progress_label.setText("Started!")
+                QApplication.processEvents()
+                
+                for count, r in enumerate(results):
+                    s = "Frame: " + str(count)
+                    self.ui.scoreboard_progress_label.setText(s)
+                    self.ui.scoreboard_progress_label.adjustSize()
+                    QApplication.processEvents()
+                    try:
+                        boxes.append(r.boxes.xyxy[0].tolist())
+                    except:
+                        boxes.append(boxes[-1])
+                        
+                # boxes_df = pd.DataFrame(boxes)
+                # boxes_df = boxes_df.astype('int')
+                # boxes_df.to_csv('boxes.csv', index=False, header=False)
+                
+                os.chdir(mycwd)
             except:
                 msg = QtWidgets.QMessageBox()
                 msg.setWindowTitle("Error occured")
